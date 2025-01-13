f863327a0c2a30228e52c3eb3c48115188f073ea
Anu Kumar <128023216+aneurokumar@users.noreply.github.com>
2025-01-13 11:38:37 +0100

    Update quartz.config.ts
    add: google analytics tag id

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google", tagId: "<G-PRDW2591JG>"
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
